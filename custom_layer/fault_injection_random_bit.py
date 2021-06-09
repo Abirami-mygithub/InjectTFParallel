ee79487f7f9456734c1ee8ff3808e9a6d0dd6cb7
abi <abiramiravi.1001@gmail.com>
2021-06-09 07:28:04 +0200

    Modified Custom layers

--- custom_layer/fault_injection_random_bit.py
+++ custom_layer/fault_injection_random_bit.py
@@ -1,3 +1,11 @@
+"""
+Filename:           fault_injection_random_bit.py 
+File Description:   A custom layer called Fault_Injector_Random_Bit is created. This custom layer takes input from the previous layer that it is connected to and 
+                    injects fault into random bits. The percentage of fault injection is controlled by a parameter called as probability. 
+Created by:         Abirami Ravi - University of Stuttgart (abirami1429@gmail.com)
+"""
+
+
 import tensorflow as tf
 import numpy as np
 
@@ -5,84 +13,53 @@ class Fault_Injector_Random_Bit(tf.keras.layers.Layer):
     def __init__(self, probability):
         super(Fault_Injector_Random_Bit, self).__init__()
         self.probability = probability
-
+        self.scale = tf.Variable(100000000000000000.)
 
     def call(self, inputs):
-        print("input tensor:", inputs)
 
-        #To flip random bit, formula is: num xor (1 << random_bit)
-        #convert the received input to int32 or int64 to make bit modifications
-        if inputs.dtype == tf.float32:
-            converted_inputs = tf.cast(inputs, dtype=tf.int32)
-
-            shape_input = tf.shape(inputs, out_type=tf.int32)
-
-            #resultant_tensor = self.__generate_tensor_probability(inputs, data_type =tf.int32)
-            #create a tensor of probability values
-            probability_tensor = tf.fill(dims= shape_input, value=self.probability)
-
-            #create a random probability tensor
-            random_tensor = tf.random.uniform(shape = shape_input, minval=0, maxval=1, dtype=tf.float32)
-
-            #compare probability tensor with the random probability tensor
-            resultant_tensor = tf.math.greater(probability_tensor, random_tensor, name=None)
-            resultant_tensor = tf.cast(resultant_tensor,dtype=tf.int32)
-
-            #generate random
-            random_bit = np.random.randint(0, 32)
-
-        elif inputs.dtype == tf.float64:
-            converted_inputs = tf.cast(inputs, dtype=tf.int64)
-
-            #create a tensor of probability values
-            probability_tensor = tf.fill(dims= shape_input, value=self.probability)
-
-            #create a random probability tensor
-            random_tensor = tf.random.uniform(shape = shape_input, minval=0, maxval=1, dtype=tf.float64)
-
-            #compare probability tensor with the random probability tensor
-            resultant_tensor = tf.math.greater(probability_tensor, random_tensor, name=None)
-            resultant_tensor = tf.cast(resultant_tensor,dtype=tf.int64)
-
-            #generate random
-            random_bit = np.random.randint(0, 64)
+        if (inputs.dtype == tf.float32):
+            input_datatype = tf.float32
+            max_limit = 32
         else:
-            raise NotImplementedError(
-                "Bit flip is not supported for dtype: ", element_val.dtype)
+            input_datatype = tf.float64
+            max_limit = 64
+            
+        #copy the inputs to this layer and also its shape
+        temp = inputs
+        input_shape = tf.shape(inputs, out_type=tf.int64)
 
-        #create a random bit tensor
-        random_bit_tensor = tf.fill(dims=shape_input, value=random_bit)
+        #Usually the layer input datatype will be float32 or float64. These float values are scaled before converting to integers.
+        temp = tf.reshape(temp, [-1]) * self.scale 
 
+
+        #Store the shape of the reshaped and scaled.
+        shape_input = tf.shape(temp, out_type=tf.int64)
+
+        #Conversion to integer
+        temp_int = tf.cast(temp, dtype=tf.int64)
+
+        #Probability tensor is created with the probability values filled
+        probability_tensor = tf.fill(dims= shape_input, value=self.probability)
+
+        #Random probability values are generated
+        random_tensor = tf.random.uniform(shape = shape_input, minval=0, maxval=1.0, dtype=input_datatype)
+
+        #Resultant tensor is created with values only in places where probability value is greater than the random value
+        resultant_tensor = tf.math.greater(probability_tensor, random_tensor, name=None)
+        resultant_tensor = tf.cast(resultant_tensor,dtype=tf.int64)
+
+        random_bit = np.random.randint(0, max_limit)
+        random_bit_tensor = tf.cast(tf.fill(dims=shape_input, value=random_bit), dtype=tf.int64)
 
         left_shift_result = tf.bitwise.left_shift(resultant_tensor, random_bit_tensor)
 
-        output = tf.bitwise.bitwise_xor(converted_inputs, left_shift_result)
-        output = tf.cast(output, dtype=inputs.dtype)
+        output = tf.bitwise.bitwise_xor(temp_int, left_shift_result)
 
+        float_output = tf.cast(output, dtype=input_datatype)
+        #print("float_output", float_output)
+        output = float_output /self.scale
+        output = tf.reshape(output, shape=input_shape)
         return output
 
     def get_config(self):
         return {"random_bit_tensor": random_bit_tensor, "probability": self.probability, "name": "fault_injector_random_bit"}
-
-    def __generate_tensor_probability(self, inputs, data_type):
-
-        resultant_tensor = tf.zeros_like(inputs)
-        shape_input = tf.shape(inputs, out_type=data_type)
-        iter_shape = (32, inputs.shape[1], inputs.shape[2], inputs.shape[3])
-        indice = []
-        for i in range(32):
-            element = []
-            random_number = np.random.rand()
-        if(self.probability > random_number):
-            for dim in iter_shape:
-                element.append(np.int32(np.random.randint(0, dim)))
-            #print(element)
-        zero_np = np.zeros(iter_shape)
-
-        #Initialize with zeros and then append values 1 in the indexes chosen
-        resultant_tensor = tf.Variable(zero_np, shape=iter_shape, name="a", dtype=data_type)
-        for index in indice:
-            zero_np[index] = 1
-            resultant_tensor.assign_add(zero_np)
-
-        return resultant_tensor
--- custom_layer/fault_injection_specific_bit.py
+++ custom_layer/fault_injection_specific_bit.py
@@ -1,40 +1,62 @@
+"""
+Filename:           fault_injection_specific_bit.py 
+File Description:   A custom layer called Fault_Injector_Specific_Bit is created. This custom layer takes input from the previous layer that it is connected to and 
+                    injects fault into specific bit numbers. The bit number is mentioned as a parameter while creating custom layer.
+                    Additionally, the percentage of fault injection is controlled by a parameter called as probability. 
+Created by:         Abirami Ravi - University of Stuttgart (abirami1429@gmail.com)
+"""
+
 import tensorflow as tf
 
 class Fault_Injector_Specific_Bit(tf.keras.layers.Layer):
-    def __init__(self, specific_bit, probability):
+    def __init__(self, probability, bit_number):
         super(Fault_Injector_Specific_Bit, self).__init__()
-        self.specific_bit = specific_bit
         self.probability = probability
+        self.scale = tf.Variable(100000000000000000.)
+        self.bit_number = bit_number
 
     def call(self, inputs):
-      print("input tensor:", inputs)
 
-      #To flip specific bit formula is: num xor (1 << specifc_bit)
-      #convert the received input to int32 or int64 to make bit modifications
-      converted_inputs = tf.cast(inputs, dtype=tf.int32)
+      if (inputs.dtype == tf.float32):
+            input_datatype = tf.float32
+        else:
+            input_datatype = tf.float64
 
-      shape_input = tf.shape(inputs, out_type=tf.int32)
+        #copy the inputs to this layer and also its shape
+        temp = inputs
+        input_shape = tf.shape(inputs, out_type=tf.int64)
 
-      #create a tensor of probability values
-      probability_tensor = tf.fill(dims= shape_input, value=self.probability)
-
-      #create a random probability tensor
-      random_tensor = tf.random.uniform(shape = shape_input, minval=0, maxval=1, dtype=tf.float32)
-
-      #compare probability tensor with the random probability tensor
-      resultant_tensor = tf.math.greater(probability_tensor, random_tensor, name=None)
-      resultant_tensor = tf.cast(resultant_tensor,dtype=tf.int32)
-
-      #create a specific bit tensor
-      specific_bit_tensor = tf.fill(dims=shape_input, value=self.specific_bit)
+        #Usually the layer input datatype will be float32 or float64. These float values are scaled before converting to integers.
+        temp = tf.reshape(temp, [-1]) * self.scale 
 
 
-      left_shift_result = tf.bitwise.left_shift(resultant_tensor, specific_bit_tensor)
+        #Store the shape of the reshaped and scaled.
+        shape_input = tf.shape(temp, out_type=tf.int64)
 
-      output = tf.bitwise.bitwise_xor(converted_inputs, left_shift_result)
-      output = tf.cast(output, dtype=inputs.dtype)
+        #Conversion to integer
+        temp_int = tf.cast(temp, dtype=tf.int64)
 
-      return output
+        #Probability tensor is created with the probability values filled
+        probability_tensor = tf.fill(dims= shape_input, value=self.probability)
+
+        #Random probability values are generated
+        random_tensor = tf.random.uniform(shape = shape_input, minval=0, maxval=1.0, dtype=input_datatype)
+
+        #Resultant tensor is created with values only in places where probability value is greater than the random value
+        resultant_tensor = tf.math.greater(probability_tensor, random_tensor, name=None)
+        resultant_tensor = tf.cast(resultant_tensor,dtype=tf.int64)
+
+        random_bit_tensor = tf.cast(tf.fill(dims=shape_input, value=self.bit_number), dtype=tf.int64)
+
+        left_shift_result = tf.bitwise.left_shift(resultant_tensor, random_bit_tensor)
+
+        output = tf.bitwise.bitwise_xor(temp_int, left_shift_result)
+
+        float_output = tf.cast(output, dtype=input_datatype)
+        #print("float_output", float_output)
+        output = float_output /self.scale
+        output = tf.reshape(output, shape=input_shape)
+        return output
 
     def get_config(self):
-        return {"specific_bit": self.specific_bit, "probability": self.probability, "name": "fault_injector_specific_bit"}
+        return {"specific_bit_tensor": random_bit_tensor, "probability": self.probability, "name": "fault_injector_specific_bit"}
